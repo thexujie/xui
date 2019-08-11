1cb284eb9825d11132eb2388c3c97dffbf9d8481
xujie <xujie@xujie.com>
2019-08-11 15:48:48 +0800

    1

--- dx12/hair/data/shaders/hair_simulate.hlsl
+++ dx12/hair/data/shaders/hair_simulate.hlsl
@@ -1,8 +1,14 @@
-#pragma pack_matrix(row_major)
+
+struct Vertex
+{
+    float4 position;
+    float2 uv;
+};
 
 cbuffer SceneConstantBuffer : register(b0)
 {
-    float4x4 transform;
+    row_major float4x4 transform;
+    float gravityAcceleration;
     float gravityStrength;
     float timeElapse;
     float angularStiffness;
@@ -11,18 +17,13 @@ cbuffer SceneConstantBuffer : register(b0)
     int numConstraintIterations;
 };
 
-struct Vertex
-{
-    float4 position;
-    float2 uv;
-};
-
 RWStructuredBuffer<Vertex> currPositions : register(u0);
 RWStructuredBuffer<float4> prevPositions : register(u1);
 
 Buffer<uint> strandOffsets : register(t0);
-// liner length / line stiffness / anguar length/ angular stiffness
+// liner length / line stiffness / anguar length/ angular stiffnessr
 Buffer<float4> constraints : register(t1);
+Buffer<float4> initPositions : register(t2);
 
 #define BLOCK_SIZE 64
 groupshared float4 sharedPos[BLOCK_SIZE];
@@ -34,43 +35,35 @@ float4 addForcesAndIntegrate(float4 position, float4 oldPosition, float3 force, 
     // 根控制点保持固定
     if (position.w == 0)
         return position;
-    
-    float internalSpeedCoefficient = stiffness * (0.05 - 0.02) + 0.02;
-    float3 velocity = (1 - internalSpeedCoefficient) * (position.xyz - oldPosition.xyz);
-    
-    //if velocity is very low increase the stiffness
+
+    float3 velocity = (position.xyz - oldPosition.xyz) * lerp(0.95f, 0.98f, stiffness);
+    // 低速时加大刚度
+    float stiffness0 = stiffness;
     if (length(velocity) < 0.01)
     {
-        float stiffness_clamped = clamp(stiffness * 5.0, 0, 1);
-        float stiffnessIncrease = stiffness_clamped - stiffness;
-        if (stiffnessIncrease > 0)
-            force *= stiffnessIncrease * 6;
+        stiffness = saturate(stiffness * 5.0);
+        if (stiffness - stiffness0 > 0)
+            stiffness0 *= (stiffness - stiffness0) * 6;
     }
+    else if (length(velocity) > 0.5f)
+    {
+        stiffness *= 0.5f;
+        stiffness0 *= 0.5f;
+    }
+    else {}
 
-    //if velocity is very high decrease the stiffness
-    if (length(velocity) > 0.5) 
-        force *= 0.5;
-    
-    float gravityStrength = (1 - stiffness) * (1.2 - 0.07) + 0.07;
-    gravityStrength *= gravityStrength;
-    
-    // Gravity
-    force += float3(0, -gravityStrength, 0);
-    
-    float4 result = float4(0, 0, 0, position.w);
-    result.xyz = position.xyz + velocity.xyz + force * timeElapse * timeElapse;
-    
-    float4 transformedPosition = float4(mul(float4(currPositions[globalIndex].position.xyz, 1), transform).xyz, currPositions[globalIndex].position.w);
-    float staticky = stiffness * 0.0475;
-
-    if (length(result.xyz - transformedPosition.xyz) < 1.6)
-        result.xyz = (1 - staticky) * result.xyz + staticky * transformedPosition.xyz;
-    return result;
+    force += float3(0.0f, -gravityAcceleration * lerp(1.0f, 0.5f, stiffness), 0.0f);
+    float3 result = position.xyz + velocity + force * timeElapse * timeElapse / 20.0f * 400.0f;
+    float3 transformedPos = mul(float4(initPositions[globalIndex].xyz, 1), transform).xyz;
+    float staticky = stiffness0 * 0.0475;
+    if (length(result - transformedPos) < 1.6)
+        result = lerp(result, transformedPos, staticky);
+    return float4(transformedPos, position.w);
 }
 
 bool IsFree(float4 particle)
 {
-    return particle.w > 1;
+    return particle.w > 0;
 }
 
 float2 Responsiveness(float4 particle0, float4 particle1)
@@ -91,6 +84,18 @@ float2 Responsiveness(float4 particle0, float4 particle1)
     }
 }
 
+void DistanceConstraint2(inout float4 particle0, inout float4 particle1, float2 constrain)
+{
+    float3 delta = particle1.xyz - particle0.xyz;
+    float distance = max(length(delta), 1e-7);
+    float stretching = 1 - constrain.x / distance;
+    delta = stretching * delta;
+    float2 responsiveness = Responsiveness(particle0, particle1);
+    particle0.xyz += responsiveness[0] * delta * constrain.y;
+    particle1.xyz -= responsiveness[1] * delta * constrain.y;
+}
+
+
 void DistanceConstraint(inout float4 particle0, inout float4 particle1, float targetDistance, float stiffness = 1.0)
 {
     float3 delta = particle1.xyz - particle0.xyz;
@@ -108,7 +113,7 @@ void CSMain(uint localIndex : SV_GroupIndex, uint3 groupId : SV_GroupID, uint3 d
     uint indexBase = groupId.x > 0 ? strandOffsets[groupId.x - 1] : 0;
     uint globalIndex = indexBase + localIndex;
     uint count = strandOffsets[groupId.x] - indexBase;
-    
+
     float4 orgPosition = float4(0, 0, 0, 0);
     if (localIndex < count)
     {
@@ -125,12 +130,11 @@ void CSMain(uint localIndex : SV_GroupIndex, uint3 groupId : SV_GroupID, uint3 d
     uint _half3 = floor((count - 3) / 2.0f);
     uint halfAng1 = max(0, _half2 - 3); //being conservative in how many particles we apply the angular constraint to
     uint halfAng2 = max(0, _half3 - 3); //being conservative in how many particles we apply the angular constraint to
-    float stiffness = 0.0f;
 
     if (localIndex < count)
     {
         sharedPos[localIndex] = addForcesAndIntegrate(sharedPos[localIndex], prevPositions[globalIndex], sharedForce[localIndex], constraints[globalIndex].y, globalIndex);
-        prevPositions[globalIndex] = orgPosition;
+        sharedPos[localIndex].xyz = mul(float4(initPositions[globalIndex].xyz, 1), transform).xyz;
     }
     
     GroupMemoryBarrierWithGroupSync();
@@ -156,7 +160,7 @@ void CSMain(uint localIndex : SV_GroupIndex, uint3 groupId : SV_GroupID, uint3 d
         //apply the angular constraints to the first subset
         if (localIndex < halfAng1)
         {
-            stiffness = sharedConstraints[id].w * angularStiffness;
+            float stiffness = sharedConstraints[id].w * angularStiffness;
             if (stiffness > 0)
                 DistanceConstraint(sharedPos[id], sharedPos[id + 2], sharedConstraints[id].z, stiffness);
         }
@@ -166,7 +170,7 @@ void CSMain(uint localIndex : SV_GroupIndex, uint3 groupId : SV_GroupID, uint3 d
         if (localIndex < halfAng2)
         {
             id += 2;
-            stiffness = sharedConstraints[id].w * angularStiffness;
+            float stiffness = sharedConstraints[id].w * angularStiffness;
             if (stiffness > 0)
                 DistanceConstraint(sharedPos[id], sharedPos[id + 2], sharedConstraints[id].z, stiffness);
         }
@@ -176,6 +180,7 @@ void CSMain(uint localIndex : SV_GroupIndex, uint3 groupId : SV_GroupID, uint3 d
     if (localIndex < count)
     {
         currPositions[globalIndex].position = sharedPos[localIndex];
+        prevPositions[globalIndex] = orgPosition;
     }
 }
 
